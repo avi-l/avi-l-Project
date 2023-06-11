49be86382d804208109c6484640f27d6949efce7
Avi Leeker <avi.leeker@gmail.com>
2023-06-12 00:24:17 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/lotr-task/",
-  build: {
-    outDir: "docs",
-    assetsDir: "assets", // Specify the assets directory
-    rollupOptions: {
-      input: {
-        index: "/index.html",
-      },
-    },
-  },
+  base: "/",
 });
