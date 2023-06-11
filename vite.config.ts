ae88750ecaa6f7a33bce64f304cf6a5d0fdaf186
Avi Leeker <avi.leeker@gmail.com>
2023-06-12 00:34:50 +0300

    gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/lotr-task",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/lotr-task/";
+  }
+
+  return config;
 });
